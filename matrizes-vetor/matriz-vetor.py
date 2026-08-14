a94deaa188d69acc5274c6229930edc324285a7b
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-08-14 11:19:59 -0300

    Refactor matriz-vetor.py to comment out initial matrix setup and add matriz creation function

--- matrizes-vetor/matriz-vetor.py
+++ matrizes-vetor/matriz-vetor.py
@@ -1,7 +1,11 @@
-lista1 = [1,2,3]
-lista2 = [4,5,6]
-lista3 = [7,8,9]
-matriz = [lista1, lista2, lista3]
+#lista1 = [1,2,3]
+#lista2 = [4,5,6]
+#lista3 = [7,8,9]
+#matriz = [lista1, lista2, lista3]
 
-print(matriz[0][0])
-matriz[0][0] = 100
+#print(matriz[0][0])
+#matriz[0][0] = 100
+
+#função criar e retornar matriz
+def criar_matriz(n_linhas,n_colunas):
+    matriz = []
